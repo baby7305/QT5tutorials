097a76af5c851f0732f371cc502172751ea662ec
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 18:43:43 +0800

    QSet

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,16 +1,38 @@
-#include <QTextStream>
+#include <QSet>
 #include <QList>
+#include <QTextStream>
+#include <algorithm>
 
 int main(void) {
 
 	QTextStream out(stdout);
 
-	QString string = "coin, book, cup, pencil, clock, bookmark";
-	QStringList items = string.split(",");
-	QStringListIterator it(items);
+	QSet<QString> cols1 = { "yellow", "red", "blue" };
+	QSet<QString> cols2 = { "blue", "pink", "orange" };
 
-	while (it.hasNext()) {
-		out << it.next().trimmed() << endl;
+	out << "There are " << cols1.size() << " values in the set" << endl;
+
+	cols1.insert("brown");
+
+	out << "There are " << cols1.size() << " values in the set" << endl;
+
+	cols1.unite(cols2);
+
+	out << "There are " << cols1.size() << " values in the set" << endl;
+
+	for (QString val : cols1) {
+		out << val << endl;
+	}
+
+	QList<QString> lcols = cols1.values();
+	std::sort(lcols.begin(), lcols.end());
+
+	out << "*********************" << endl;
+	out << "Sorted:" << endl;
+
+	for (QString val : lcols) {
+		out << val << endl;
 	}
 	getchar();
+	return 0;
 }
