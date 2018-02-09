cf8e1743abdb81006beaa61fee9402d5e4479701
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 18:27:07 +0800

    julianday day

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,13 +1,13 @@
 #include <QTextStream>
-#include <QDateTime>
+#include <QDate>
 
 int main(void) {
 
 	QTextStream out(stdout);
-	QDateTime cdt = QDateTime::currentDateTime();
 
-	out << "The current datetime is " << cdt.toString() << endl;
-	out << "The current date is " << cdt.date().toString() << endl;
-	out << "The current time is " << cdt.time().toString() << endl;
+	QDate cd = QDate::currentDate();
+
+	out << "Gregorian date for today: " << cd.toString(Qt::ISODate) << endl;
+	out << "Julian day for today: " << cd.toJulianDay() << endl;
 	getchar();
 }
