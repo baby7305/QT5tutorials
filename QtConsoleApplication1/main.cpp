e52002aac3730c3c445ccd31ea68a6a9e4293b6b
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 17:08:04 +0800

    时间日期的格式化输出

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,8 +1,23 @@
-#include <QtCore>
-#include <iostream>
+#include <QTextStream>
+#include <QDate>
+#include <QTime>
 
-int main() {
+int main(void) {
 
-	std::cout << "Qt version: " << qVersion() << std::endl;
+	QTextStream out(stdout);
+
+	QDate dt1(2015, 4, 12);
+	out << "The date is " << dt1.toString() << endl;
+
+	QDate dt2;
+	dt2.setDate(2015, 3, 3);
+	out << "The date is " << dt2.toString() << endl;
+
+	QTime tm1(17, 30, 12, 55);
+	out << "The time is " << tm1.toString("hh:mm:ss.zzz") << endl;
+
+	QTime tm2;
+	tm2.setHMS(13, 52, 45, 155);
+	out << "The time is " << tm2.toString("hh:mm:ss.zzz") << endl;
 	getchar();
 }
