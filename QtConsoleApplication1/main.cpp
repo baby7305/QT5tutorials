6d922ba63e00468820c48f4ca0220623e82c0a9d
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 18:20:32 +0800

    验证日期有效性

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -4,39 +4,18 @@
 int main(void) {
 
 	QTextStream out(stdout);
-	QList<QString> months;
 
-	months.append("January");
-	months.append("February");
-	months.append("March");
-	months.append("April");
-	months.append("May");
-	months.append("June");
-	months.append("July");
-	months.append("August");
-	months.append("September");
-	months.append("October");
-	months.append("November");
-	months.append("December");
+	QList<QDate> dates({ QDate(2015, 5, 11), QDate(2015, 8, 1),
+		QDate(2015, 2, 30) });
 
-	QDate dt1(2015, 9, 18);
-	QDate dt2(2015, 2, 11);
-	QDate dt3(2015, 5, 1);
-	QDate dt4(2015, 12, 11);
-	QDate dt5(2015, 1, 21);
+	for (int i = 0; i < dates.size(); i++) {
 
-	out << "There are " << dt1.daysInMonth() << " days in "
-		<< months.at(dt1.month() - 1) << endl;
-	out << "There are " << dt2.daysInMonth() << " days in "
-		<< months.at(dt2.month() - 1) << endl;
-	out << "There are " << dt3.daysInMonth() << " days in "
-		<< months.at(dt3.month() - 1) << endl;
-	out << "There are " << dt4.daysInMonth() << " days in "
-		<< months.at(dt4.month() - 1) << endl;
-	out << "There are " << dt5.daysInMonth() << " days in "
-		<< months.at(dt5.month() - 1) << endl;
-
-	out << "There are " << dt1.daysInYear() << " days in year "
-		<< QString::number(dt1.year()) << endl;
+		if (dates.at(i).isValid()) {
+			out << "Date " << i + 1 << " is a valid date" << endl;
+		}
+		else {
+			out << "Date " << i + 1 << " is not a valid date" << endl;
+		}
+	}
 	getchar();
 }
