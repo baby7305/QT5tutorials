23203c4883e7d73638f55877c0043888971cb2c7
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 18:32:24 +0800

    unix时间

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,13 +1,19 @@
 #include <QTextStream>
 #include <QDateTime>
+#include <ctime>
 
 int main(void) {
 
 	QTextStream out(stdout);
 
-	QDateTime cdt = QDateTime::currentDateTime();
+	time_t t = time(0);
+	out << t << endl;
 
-	out << "Universal datetime: " << cdt.toUTC().toString() << endl;
-	out << "Local datetime: " << cdt.toLocalTime().toString() << endl;
+	QDateTime dt;
+	dt.setTime_t(t);
+	out << dt.toString() << endl;
+
+	QDateTime cd = QDateTime::currentDateTime();
+	out << cd.toTime_t() << endl;
 	getchar();
 }
