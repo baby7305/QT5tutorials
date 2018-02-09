393b044ce8cb390d42e10b3c52df806cf4408d73
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 16:11:17 +0800

    字符串参数插入

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -4,15 +4,21 @@ int main(void) {
 
 	QTextStream out(stdout);
 
-	QString s1 = "Eagle";
-	QString s2 = "Eagle\n";
-	QString s3 = "Eagle ";
-	QString s4 = "§à§â§Ö§Ý";
+	QString s1 = "There are %1 white roses";
+	int n = 12;
 
-	out << s1.length() << endl;
-	out << s2.length() << endl;
-	out << s3.length() << endl;
-	out << s4.length() << endl;
+	out << s1.arg(n) << endl;
+
+	QString s2 = "The tree is %1 m high";
+	double h = 5.65;
+
+	out << s2.arg(h) << endl;
+
+	QString s3 = "We have %1 lemons and %2 oranges";
+	int ln = 12;
+	int on = 4;
+
+	out << s3.arg(ln).arg(on) << endl;
 	getchar();
 	return 0;
 }
