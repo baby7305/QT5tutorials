245b06119f71db3d333fe82e4639bc8e2cf26c87
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 18:42:01 +0800

    QStringList

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,29 +1,16 @@
 #include <QTextStream>
 #include <QList>
-#include <algorithm>
 
 int main(void) {
 
 	QTextStream out(stdout);
 
-	QList<QString> authors = { "Balzac", "Tolstoy",
-		"Gulbranssen", "London" };
+	QString string = "coin, book, cup, pencil, clock, bookmark";
+	QStringList items = string.split(",");
+	QStringListIterator it(items);
 
-	for (int i = 0; i < authors.size(); ++i) {
-
-		out << authors.at(i) << endl;
-	}
-
-	authors << "Galsworthy" << "Sienkiewicz";
-
-	out << "***********************" << endl;
-
-	std::sort(authors.begin(), authors.end());
-
-	out << "Sorted:" << endl;
-	for (QString author : authors) {
-
-		out << author << endl;
+	while (it.hasNext()) {
+		out << it.next().trimmed() << endl;
 	}
 	getchar();
 }
