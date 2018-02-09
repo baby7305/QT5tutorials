b2e70beefba3334c6a5f533ad1da765ecf4445c5
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-09 18:28:41 +0800

    根据日历计算天数

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -5,9 +5,16 @@ int main(void) {
 
 	QTextStream out(stdout);
 
+	QDate bordate(1812, 9, 7);
+	QDate slavdate(1805, 12, 2);
+
 	QDate cd = QDate::currentDate();
 
-	out << "Gregorian date for today: " << cd.toString(Qt::ISODate) << endl;
-	out << "Julian day for today: " << cd.toJulianDay() << endl;
+	int j_today = cd.toJulianDay();
+	int j_borodino = bordate.toJulianDay();
+	int j_slavkov = slavdate.toJulianDay();
+
+	out << "Days since Slavkov battle: " << j_today - j_slavkov << endl;
+	out << "Days since Borodino battle: " << j_today - j_borodino << endl;
 	getchar();
 }
