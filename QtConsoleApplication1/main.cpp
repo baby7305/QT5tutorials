a07bd5e86f08c8bdcedefabab1e4ffe181451ce2
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-11 10:45:36 +0800

    获取目录内容

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,5 +1,6 @@
 #include <QTextStream>
-#include <QFile>
+#include <QFileInfo>
+#include <QDir>
 
 int main(int argc, char *argv[]) {
 
@@ -7,80 +8,49 @@ int main(int argc, char *argv[]) {
 
 //	if (argc != 2) {
 //
-//		out << "Usage: permissions file" << endl;
+//		qWarning("Usage: list_dir directory");
 //		return 1;
 //	}
 
-//	QString filename = argv[1];
-	QString filename = "F:/test.txt";
+//	QString directory = argv[1];
+	QString directory = "F:/sublimeCode";
 
-	QFile::Permissions ps = QFile::permissions(filename);
+	QDir dir(directory);
 
-	QString fper;
-
-	if (ps & QFile::ReadOwner) {
-		fper.append('r');
-	}
-	else {
-		fper.append('-');
+	if (!dir.exists()) {
+		qWarning("The directory does not exist");
+		return 1;
 	}
 
-	if (ps & QFile::WriteOwner) {
-		fper.append('w');
-	}
-	else {
-		fper.append('-');
+	dir.setFilter(QDir::Files | QDir::AllDirs);
+	dir.setSorting(QDir::Size | QDir::Reversed);
+
+	QFileInfoList list = dir.entryInfoList();
+
+	int max_size = 0;
+
+	foreach(QFileInfo finfo, list) {
+
+		QString name = finfo.fileName();
+		int size = name.size();
+
+		if (size > max_size) {
+
+			max_size = size;
+		}
 	}
 
-	if (ps & QFile::ExeOwner) {
-		fper.append('x');
-	}
-	else {
-		fper.append('-');
-	}
+	int len = max_size + 2;
 
-	if (ps & QFile::ReadGroup) {
-		fper.append('r');
-	}
-	else {
-		fper.append('-');
-	}
+	out << QString("Filename").leftJustified(len).append("Bytes") << endl;
 
-	if (ps & QFile::WriteGroup) {
-		fper.append('w');
-	}
-	else {
-		fper.append('-');
-	}
+	for (int i = 0; i < list.size(); ++i) {
 
-	if (ps & QFile::ExeGroup) {
-		fper.append('x');
+		QFileInfo fileInfo = list.at(i);
+		QString str = fileInfo.fileName().leftJustified(len);
+		str.append(QString("%1").arg(fileInfo.size()));
+		out << str << endl;
 	}
-	else {
-		fper.append('-');
-	}
-
-	if (ps & QFile::ReadOther) {
-		fper.append('r');
-	}
-	else {
-		fper.append('-');
-	}
-
-	if (ps & QFile::WriteOther) {
-		fper.append('w');
-	}
-	else {
-		fper.append('-');
-	}
-
-	if (ps & QFile::ExeOther) {
-		fper.append('x');
-	}
-	else {
-		fper.append('-');
-	}
-
-	out << fper << endl;
 	getchar();
+	return 0;
 }
