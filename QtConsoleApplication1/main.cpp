0fd29ac814f0b45cc828d43cf8b8504abdbacfe6
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-11 11:10:36 +0800

    退出按钮测试

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,45 +1,30 @@
 #include <QApplication>
 #include <QWidget>
-#include <QFrame>
-#include <QGridLayout>
+#include <QPushButton>
 
-class Cursors : public QWidget {
+class MyButton : public QWidget {
 
 public:
-	Cursors(QWidget *parent = 0);
+	MyButton(QWidget *parent = 0);
 };
 
-Cursors::Cursors(QWidget *parent)
+MyButton::MyButton(QWidget *parent)
 	: QWidget(parent) {
 
-	QFrame *frame1 = new QFrame(this);
-	frame1->setFrameStyle(QFrame::Box);
-	frame1->setCursor(Qt::SizeAllCursor);
+	QPushButton *quitBtn = new QPushButton("Quit", this);
+	quitBtn->setGeometry(50, 40, 75, 30);
 
-	QFrame *frame2 = new QFrame(this);
-	frame2->setFrameStyle(QFrame::Box);
-	frame2->setCursor(Qt::WaitCursor);
-
-	QFrame *frame3 = new QFrame(this);
-	frame3->setFrameStyle(QFrame::Box);
-	frame3->setCursor(Qt::PointingHandCursor);
-
-	QGridLayout *grid = new QGridLayout(this);
-	grid->addWidget(frame1, 0, 0);
-	grid->addWidget(frame2, 0, 1);
-	grid->addWidget(frame3, 0, 2);
-
-	setLayout(grid);
+	connect(quitBtn, &QPushButton::clicked, qApp, &QApplication::quit);
 }
 
 int main(int argc, char *argv[]) {
 
 	QApplication app(argc, argv);
 
-	Cursors window;
+	MyButton window;
 
-	window.resize(350, 150);
-	window.setWindowTitle("Cursors");
+	window.resize(250, 150);
+	window.setWindowTitle("QPushButton");
 	window.show();
 
 	return app.exec();
