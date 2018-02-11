bab1ce475a7074e2944aec5e502bdbaa9117e9d4
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-11 09:32:51 +0800

    测试提取文件大小信息

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,8 +1,33 @@
-#include <QtCore>
-#include <iostream>
+#include <QTextStream>
+#include <QFileInfo>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-	std::cout << "Qt version: " << qVersion() << std::endl;
+	QTextStream out(stdout);
+
+//	if (argc != 2) {
+//
+//		qWarning("Usage: file_size file");
+//		getchar();
+//		return 1;
+//	}
+
+//	QString filename = argv[1];
+	QString filename = "test.txt";
+
+	if (!QFile(filename).exists()) {
+
+		qWarning("The file does not exist");
+		getchar();
+		return 1;
+	}
+
+	QFileInfo fileinfo(filename);
+
+	qint64 size = fileinfo.size();
+
+	QString str = "The size is: %1 bytes";
+
+	out << str.arg(size) << endl;
 	getchar();
 }
