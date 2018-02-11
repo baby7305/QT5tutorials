68e444d6b031385e67f754a272a627c277ac7f1d
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-11 10:39:07 +0800

    文件权限测试，不太懂

--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -1,5 +1,5 @@
 #include <QTextStream>
-#include <QFileInfo>
+#include <QFile>
 
 int main(int argc, char *argv[]) {
 
@@ -7,27 +7,80 @@ int main(int argc, char *argv[]) {
 
 //	if (argc != 2) {
 //
-//		out << "Usage: file_times file" << endl;
+//		out << "Usage: permissions file" << endl;
 //		return 1;
 //	}
 
 //	QString filename = argv[1];
-	QString filename = "F:\\World Machine Documents\\World Machine Documents\\startup.tmd";
+	QString filename = "F:/test.txt";
 
-	QFileInfo fileinfo(filename);
+	QFile::Permissions ps = QFile::permissions(filename);
 
-	QString absPath = fileinfo.absoluteFilePath();
-	QString baseName = fileinfo.baseName();
-	QString compBaseName = fileinfo.completeBaseName();
-	QString fileName = fileinfo.fileName();
-	QString suffix = fileinfo.suffix();
-	QString compSuffix = fileinfo.completeSuffix();
+	QString fper;
 
-	out << "Absolute file path: " << absPath << endl;
-	out << "Base name: " << baseName << endl;
-	out << "Complete base name: " << compBaseName << endl;
-	out << "File name: " << fileName << endl;
-	out << "Suffix: " << suffix << endl;
-	out << "Whole suffix: " << compSuffix << endl;
+	if (ps & QFile::ReadOwner) {
+		fper.append('r');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::WriteOwner) {
+		fper.append('w');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::ExeOwner) {
+		fper.append('x');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::ReadGroup) {
+		fper.append('r');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::WriteGroup) {
+		fper.append('w');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::ExeGroup) {
+		fper.append('x');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::ReadOther) {
+		fper.append('r');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::WriteOther) {
+		fper.append('w');
+	}
+	else {
+		fper.append('-');
+	}
+
+	if (ps & QFile::ExeOther) {
+		fper.append('x');
+	}
+	else {
+		fper.append('-');
+	}
+
+	out << fper << endl;
 	getchar();
 }
